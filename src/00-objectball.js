a808796c0903bba851bc807f801dcdd02cdd7732
AlexEnnis <alexennis373@gmail.com>
2023-09-12 16:44:26 -0400

    started lab

---
+++ src/00-objectball.js
@@ -0,0 +1,19 @@
+function gameObject (teamInfo){
+    const home ={
+        teamName:"Brooklyn Nets"
+        colors:["black","white"]
+        players:{"Allan Anderson":{
+            number:0
+            shoe:16
+            points :22
+            rebound :12
+            assist:12
+            steals:3
+            blocks :1
+            slamdunks :1
+
+
+        }}
+    
+    }
+}
